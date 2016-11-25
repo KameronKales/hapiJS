268ebb192f67b950672ead415421fc242bed5feb
Kameron <Kaleskr@appstate.edu>
2016-11-25 01:51:56 -0500

    fixed broken json file.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Hapi = require('hapi');
-const Good = require('good');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
@@ -10,38 +9,14 @@ server.route({
     method: 'GET',
     path: '/',
     handler: function(request, reply) {
-        reply('Hello, world!');
+        reply.file('index.html')
     }
 });
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function(request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
-
-server.register({
-    register: Good,
-    options: {
-        reporters: {
-            console: [{
-                module: 'good-squeeze',
-                name: 'Squeeze',
-                args: [{
-                    response: '*',
-                    log: '*'
-                }]
-            }, {
-                module: 'good-console'
-            }, 'stdout']
-        }
-    }
-}, (err) => {
+server.register(require('inert'), (err) => {
 
     if (err) {
-        throw err; // something bad happened loading the plugin
+        throw err;
     }
 
     server.start((err) => {
@@ -49,6 +24,7 @@ server.register({
         if (err) {
             throw err;
         }
-        server.log('info', 'Server running at: ' + server.info.uri);
+
+        console.log('Server running at:', server.info.uri);
     });
 });
